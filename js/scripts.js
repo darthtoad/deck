41eccca08c5b69710d123059ca1066b92ac6d9e1
Glen Buck and Sam Gespass <sam@>
2017-12-05 09:02:46 -0800

    change forEach loop into for loop

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,16 @@ $(document).ready(function(){
     var cards=["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
     var suits=["hearts", "clubs", "diamonds", "spades"];
 
-    cards.forEach(function(card){
+    for(var i=0; i<cards.length; i++){
+      for(var j=0; j<suits.length; j++) {
+        $("#deck").append(cards[i] + " of " + suits[j] + "<br>")
+      }
+    }
+
+    /*cards.forEach(function(card){
       suits.forEach(function(suit){
         $("#deck").append(card + " of " + suit + "</br>");
       });
-    });
+    });*/
   });
 });
